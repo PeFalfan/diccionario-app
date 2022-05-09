84827e23f55a0fb09f10746a64d4fd0d8254d73f
Paulysile <80928748+Paulysile@users.noreply.github.com>
2022-05-09 19:53:01 -0400

    sprint1

--- diccionario-app/src/app/pages/log-in/log-in.page.ts
+++ diccionario-app/src/app/pages/log-in/log-in.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.page.html',
   styleUrls: ['./log-in.page.scss'],
 })
 export class LogInPage implements OnInit {
-
-  constructor() { }
+clientEmailLogin: string= "";
+clientPasswordLogin: string="";
+  constructor(public navctrl: NavController) { }
 
   ngOnInit() {
   }
+
+validarLogin(){
+  var correo= this.clientEmailLogin
+  var contrasena= this.clientPasswordLogin
+  if(correo==="correo1@duocuc.cl" && contrasena==="1234"){
+    console.log("login ok")
+    this.navctrl.navigateRoot("home")
+  }else{
+    console.log("login no ok")
+  }
+}
 
 }
--- diccionario-app/src/app/pages/register-form/register-form.page.ts
+++ diccionario-app/src/app/pages/register-form/register-form.page.ts
@@ -6,10 +6,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.page.scss'],
 })
 export class RegisterFormPage implements OnInit {
+clientRut: string= "";
+clientName: string= "";
+clientLastNames: string= "";
+clientPhone: string= "";
+clientEmail: string="";
+clientPassword: string= "";
+clientRepeatPassword: string= "";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+
+
+  validarCorreo(){
+    var correo = this.clientEmail
+    if (correo.indexOf('@duocuc.cl') >= 0) {
+      console.log('correo ok')
+    }
+    else {
+      console.log('correo no ok')
+    }
+  }
+
+  validarCampos(){
+    var nombre = this.clientName
+    var apellido= this. clientLastNames
+    var fono= this.clientPhone
+    var rut= this.clientRut
+    var correo= this.clientEmail
+    var contrasena= this.clientPassword
+    var contrasena2= this.clientRepeatPassword
+    if(
+      nombre != "" && apellido != "" && fono !="" && rut != ""
+      && correo != ""&& contrasena != "" && contrasena2 != ""
+    ){
+      console.log("campos ok")
+    }else{
+      console.log("campos vacios")
+    }
+
+  }
+
+  validarFono(){
+    var fono= this.clientPhone
+    if(fono.length == 12){
+      console.log("fono ok")
+    }else{
+      console.log("fono no ok")
+    }
+  }
+  validarRut(){
+    var rut= this.clientRut
+    if(rut.length >= 9 && rut.length <= 10){
+      console.log("rut ok")
+    }else{
+      console.log("rut no ok")
+    }
+  }
+
+validarContrasena(){
+  var contrasena= this.clientPassword
+  console.log("texto"+contrasena)
+  var regex = /^(?=.[A-Za-z])(?=.\d)(?=.[@$!%#?&]){8,}$/
+  if (regex.test(contrasena)) {
+        console.log("contraseña ok")
+      }
+  else {
+        console.log("contraseña no ok")
+      }
+  }
+
+
+ validarContrasena2(){
+   var contrasena= this.clientPassword
+   var contrasena2= this.clientRepeatPassword
+   if(contrasena===contrasena2){
+     console.log("contrasena iguales")
+   }else{
+     console.log("contrasena distintas")
+   }
+ }
 }
