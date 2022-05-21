a309d4958023ab1e14f9e23b8c54d2b38b5a9983
Pedro Falfan <pe.falfan@duocuc.cl>
2022-05-20 20:28:40 -0400

    HotFIx - import fix for httpClient

--- diccionario-app/src/app/app.module.ts
+++ diccionario-app/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { SQLite } from '@ionic-native/sqlite/ngx';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
