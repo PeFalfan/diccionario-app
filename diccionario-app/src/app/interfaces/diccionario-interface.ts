e4fb36ee59925154a0ce0a6ed1b43d7d08b26c5d
Cristobal <cris.avendano88@gmail.com>
2022-05-27 20:43:00 -0400

    commit 27-05-2022

---
+++ diccionario-app/src/app/interfaces/diccionario-interface.ts
@@ -0,0 +1,6 @@
+export interface IPalabra {
+
+    traslation: string,
+    word: string,
+
+}
--- diccionario-app/src/app/pages/new-word/new-word.page.ts
+++ diccionario-app/src/app/pages/new-word/new-word.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-new-word',
@@ -7,9 +8,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewWordPage implements OnInit {
 
-  constructor() { }
+  palabraEsp: string = ''
+  palabraRapa: string = ''
+
+  constructor(private alertController: AlertController, public navController: NavController) { }
 
   ngOnInit() {
+  }
+
+  cancelarPalabra() {
+    this.navController.navigateRoot('home')
+  }
+
+  limpiarFormulario() {
+    this.palabraEsp = ''
+    this.palabraRapa = ''
+  }
+
+  guardarPalabra() {
+    var palabraEsp = this.palabraEsp
+    var palabraRapa = this.palabraRapa;
+
+    if (palabraEsp == '' || palabraRapa == '') {
+      this.camposVacios();
+    }
+    else {
+      this.AlertaPalabra();
+    }
+  }
+
+  async AlertaPalabra() {
+    console.log('palabra Español', this.palabraEsp)
+    console.log('palabra Rapa', this.palabraRapa)
+    const alert = await this.alertController.create({
+      header: 'Palabra Cargada Correctamente',
+      buttons:
+        [
+          {
+            text: 'Ir al inicio',
+            role: 'cancel',
+            cssClass: 'secondary',
+            id: 'cancel-button',
+            handler: () => {
+              console.log('Inicio');
+              this.navController.navigateRoot('home')
+            }
+          }, {
+            text: 'Cargar otra palabra',
+            id: 'confirm-button',
+            handler: () => {
+              console.log('Otra palabra');
+              window.location.reload();
+            }
+          }
+        ]
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async camposVacios() {
+    console.log('faltan palabras')
+    const alert = await this.alertController.create({
+      header: 'Por favor rellene ambos campos con palabras',
+      buttons:
+        [
+          {
+            text: 'Ir al inicio',
+            cssClass: 'secondary',
+            id: 'confirm-button',
+            handler: () => {
+              console.log('Inicio');
+              this.navController.navigateRoot('home')
+            }
+          }, {
+            text: 'Volver a cargar palabra',
+            role: 'cancel',
+            id: 'cancel-button',
+            handler: () => {
+              console.log('Otra palabra');
+            }
+          }
+        ]
+    });
+    await alert.present();
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+
   }
 
 }
